4892072232976dd5a1e2edf4337eca1528799068
Nkaka23dev <62412678+Nkaka23dev@users.noreply.github.com>
2020-07-18 19:33:57 -0700

    another commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ app=Flask(__name__)
 
 @app.route('/',methods=['POST','GET'])
 def calculate():
+   # here we declare bmi globally
    bmi=float()
    if request.method=='POST' and 'weight' in request.form and 'height' in request.form:
       try:
