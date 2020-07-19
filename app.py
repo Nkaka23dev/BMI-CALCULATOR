45d3e306534b11fe192d7e44fe674ce3dac8ad32
Nkaka23dev <62412678+Nkaka23dev@users.noreply.github.com>
2020-07-18 18:30:05 -0700

    my first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template,request
+app=Flask(__name__)
+
+@app.route('/',methods=['POST','GET'])
+def calculate():
+   bmi=float()
+   if request.method=='POST' and 'weight' in request.form and 'height' in request.form:
+      try:
+         weight=float(request.form.get('weight'))
+         height=float(request.form.get('height'))
+         bmi=round(weight/(height**2),2)
+      except Exception as e:
+         print(e) 
+   return render_template("index.html",bmi=bmi)
+
+
+if __name__=='__main__':
+    app.run(debug=True)
